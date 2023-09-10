124a4cec31410d840c00c4561fc8d8621ec28486
Jensjee <jensslauerhoff@hotmail.com>
2023-09-10 15:37:12 +0200

    Wazuh agent installatie toegevoegd

--- Suricata setup/suricata-setup.sh
+++ Suricata setup/suricata-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Vraag om Wazuh manager IP om wazuh agent te installeren
+read -p "Voer de Wazuh-manager IP in: " WAZUH_MANAGER
+
 # Update de pakketlijst om ervoor te zorgen dat je de nieuwste informatie hebt
 sudo apt update
 
@@ -35,3 +38,9 @@ sudo systemctl enable suricata
 
 # Controleer de status van Suricata om te verifiëren dat het correct is geïnstalleerd en wordt uitgevoerd
 sudo systemctl status suricata
+
+# Wazuh agent installeren voor manager
+curl -so wazuh-agent.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.4.4-1_amd64.deb && sudo WAZUH_MANAGER="$WAZUH_MANAGER" WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME='Suricata' dpkg -i ./wazuh-agent.deb
+
+# Geef een melding weer dat de installatie is voltooid
+echo "Wazuh-agent is geïnstalleerd en geconfigureerd."
